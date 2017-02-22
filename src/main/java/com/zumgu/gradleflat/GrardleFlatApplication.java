3eda529e741252c76c73900731f6c88f82593b11
kanghonggu <wckhg89@gmail.com>
2017-02-22 18:42:09 +0900

    init commit

---
+++ src/main/java/com/zumgu/gradleflat/GrardleFlatApplication.java
@@ -0,0 +1,12 @@
+package com.zumgu.gradleflat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrardleFlatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrardleFlatApplication.class, args);
+	}
+}
